0a6b5a06d9796c0482468c1220a3dbcf674c2468
hongbin-yu <hongbin.yu413@gmail.com>
2020-04-23 13:38:52 -0400

    add member.php

---
+++ members.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Show the list of current groups.
+ *
+ * @package		ProjectSend
+ * @subpackage	Groups
+ *
+ */
+
+
+$allowed_levels = array(9,8);
+require_once('sys.includes.php');
+$ROOT = '/srv/www/zerotier-one/controller.d/network/';
+
+if(!empty($_GET['nwid'])) {
+	$dir = $ROOT.$_GET['nwid'].'/member';
+	echo '[';
+	if(is_dir($dir)) {
+		if($dh = opendir($dir)) {
+			while (($file = readdir{$dh))!== false){
+				if(endsWith($file,'.json'))
+					echo readfile($file);
+			}
+		}
+		closedir($dh);
+	}
+	echo ']';
+}
+
+
+?>
+
+
+
+<?php
+
